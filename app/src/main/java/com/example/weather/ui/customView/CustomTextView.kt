594d9016e4e34cb544b1999c50bca0484c152cec
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2019-06-17 23:18:10 +0800

    add custom view

--- app/src/main/java/com/example/weather/ui/customView/CustomTextView.kt
+++ app/src/main/java/com/example/weather/ui/customView/CustomTextView.kt
@@ -13,17 +13,17 @@ class CustomTextView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : TextView(context, attrs, defStyleAttr){
     init {
-
-        LayoutInflater.from(context).inflate(R.layout.custom_view_layout, this, true)
-        attrs?.let {
-            val typedArray = context.obtainStyledAttributes(it,
-                R.styleable.custom_component_attributes, 0, 0)
-            val dayWeather = resources.getText(typedArray
-                .getResourceId(R.styleable
-                    .custom_component_attributes_dateWeather,R.string.app_name))
-
-            typedArray.recycle()
-        }
+//
+//        LayoutInflater.from(context).inflate(R.layout.custom_view_layout, this, true)
+//        attrs?.let {
+//            val typedArray = context.obtainStyledAttributes(it,
+//                R.styleable.custom_component_attributes, 0, 0)
+//            val dayWeather = resources.getText(typedArray
+//                .getResourceId(R.styleable
+//                    .custom_component_attributes_dateWeather,R.string.app_name))
+//
+//            typedArray.recycle()
+//        }
     }
 }
 
